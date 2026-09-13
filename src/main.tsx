550341e5d67deddd448f3cbb06b857c3f185e5fc
hendelkasar <hend.elkasar@gmail.com>
2026-09-13 20:42:26 +0300

    Prepare Pixora for production

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-
 import { Analytics } from '@vercel/analytics/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
@@ -14,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
       <LanguageProvider>
         <BrowserRouter>
           <App />
+          <Analytics />
         </BrowserRouter>
       </LanguageProvider>
     </ThemeProvider>
