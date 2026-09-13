862fded303a004267869852dc0679f6809828069
hendelkasar <hend.elkasar@gmail.com>
2026-09-13 20:30:22 +0300

    Add Vercel Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,5 @@
+
+import { Analytics } from '@vercel/analytics/react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
